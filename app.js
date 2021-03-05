ef32ad25162b453935bdd143d8823353054888ee
Justin_karso <justin.karsopawiro001@fclive.nl>
2021-03-05 20:52:20 +0100

    t

--- app.js
+++ app.js
@@ -7,6 +7,23 @@ const cors = require('cors');
 app.use(cors())
 app.use(express.json())
 
+const db = mysql.createConnection({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DB
+});
+
+app.post('/posts', (req, res) => {
+    db.query('SELECT * FROM designs', (err, result ) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.send(result)
+        }
+    })
+})
+
 app.use(express.static('dist'))
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/dist/index.html'))
--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
 
 const App = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.post('/posts')
+        .then(res => {
+            setData(res.data)
+        })
+    }, [])
+
     return (
         <div>
             <h1>Hello World</h1>
+            <div>
+                {data && data.map(item => {
+                    return(
+                        <div key={item.id}>
+                            <p>{item.title}</p>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
