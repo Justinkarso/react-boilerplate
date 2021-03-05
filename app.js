90705edb0a6783ebef66eadf233538d8b02814e1
Justin_karso <justin.karsopawiro001@fclive.nl>
2021-03-05 20:54:00 +0100

    t

--- app.js
+++ app.js
@@ -29,6 +29,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/dist/index.html'))
 })
 
-app.listen(5000, () => {
-    console.log('Server running on http://localhost:5000')
+app.listen(3001, () => {
+    console.log('Server running on http://localhost:3001')
 })
